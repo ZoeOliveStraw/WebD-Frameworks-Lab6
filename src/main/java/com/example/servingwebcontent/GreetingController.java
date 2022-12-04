67b94ed82b5a7a232ba3225229c46d310f858f7b
Zoe Straw <zoe.olive.straw@gmail.com>
2022-12-04 13:51:50 -0500

    Mostly finished Lab 6

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Zoe") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye(@RequestParam(name="name", required=false, defaultValue="Zoe") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "goodbye";
+    }
+}
